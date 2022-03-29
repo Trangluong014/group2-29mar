520b036aef28cec73440b70746bb7b2b0e14b080
Trang Luong <luongtrang014@gmail.com>
2022-03-29 16:16:15 +0800

    update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import CartItem from "./CartItem";
 import useCartContext from "../hooks/useCartContext";
 
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import useCartContext from "../hooks/useCartContext";
 
 function ProductItem({ product }) {
